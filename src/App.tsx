f4db994ed0e00f81f1d6fe8563a49f587c93f211
ksr03 <K_Haruka03@outlook.jp>
2024-04-24 02:50:28 +0900

    全体の大枠を作成した

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import CheckboxList from './components/CheckboxList';
+import Graph from './components/Graph';
+import Title from './components/Title';
 
 function App(): JSX.Element  {
   return (
-    <div className="App">
-      hello world
+    <div className='App'>
+      <div className='main-container'>
+        <Title/>
+        <CheckboxList/>
+        <Graph/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Title.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Title(): JSX.Element {
+  return (
+    <div className='title-wrapper'>
+      <h1 className='title'>コーディング試験</h1>
+    </div>
+  )
+}
+
+export default Title
